fc69aab42ae81bafcd913da1520ae6b0dd5e8406
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-18 22:07:41 +0530

    first commit

---
+++ src/controllers/menus/index.ts
@@ -0,0 +1,29 @@
+import { Response, Request } from "express";
+import { IMenu } from "../../types/menu";
+import Menu from "../../models/menu";
+
+const getMenus = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const menus: IMenu[] = await Menu.find();
+    res.status(200).json({ menus });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const addMenu = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const body = req.body as Pick<IMenu, "name" | "description" | "price">;
+    const menu: IMenu = new Menu({
+      name: body.name,
+      description: body.description,
+      price: body.price,
+    });
+
+    const newMenu: IMenu = await menu.save();
+
+    res.status(201).json(newMenu);
+  } catch (error) {
+    throw error;
+  }
+};
