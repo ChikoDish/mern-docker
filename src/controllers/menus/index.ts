5948c0a95a7c1be58b72b5762f8dbb99a90e3846
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-19 07:37:45 +0530

    running app

--- src/controllers/menus/index.ts
+++ src/controllers/menus/index.ts
@@ -11,6 +11,19 @@ const getMenus = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+const retrieveMenu = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const menu: IMenu | null = await Menu.findById({ _id: id });
+
+    res.status(menu ? 200 : 404).json({ menu });
+  } catch (error) {
+    throw error;
+  }
+};
+
 const addMenu = async (req: Request, res: Response): Promise<void> => {
   try {
     const body = req.body as Pick<IMenu, "name" | "description" | "price">;
@@ -27,3 +40,37 @@ const addMenu = async (req: Request, res: Response): Promise<void> => {
     throw error;
   }
 };
+
+const updateMenu = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      params: { id },
+      body,
+    } = req;
+
+    const updateMenu: IMenu | null = await Menu.findByIdAndUpdate(
+      { _id: id },
+      body
+    );
+
+    res.status(updateMenu ? 200 : 404).json({
+      menu: updateMenu,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteMenu = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const deletedMenu: IMenu | null = await Menu.findByIdAndRemove(
+      req.params.id
+    );
+    res.status(204).json({
+      todo: deletedMenu,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+export { getMenus, addMenu, updateMenu, deleteMenu, retrieveMenu };
